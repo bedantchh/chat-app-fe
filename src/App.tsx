eb47202a148a297fd18b0e1dec704c036a7fd8f3
Bedant Chhetri <bedantchhetri7@gmail.com>
2025-07-21 11:57:30 +0530

    chat working

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef, useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [messages, setMessages] = useState(["hi there","hello"])
+  const inputRef= useRef()
+  const wsRef= useRef()
+  useEffect(()=>{
+    const ws= new WebSocket("ws://localhost:8080");
+    ws.onmessage = (event)=>{
+      setMessages(messages => [...messages, event.data])
+    }
+    console.log(event.data)
+    wsRef.current= ws
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    ws.onopen = () => {
+      ws.send(JSON.stringify({
+        type: "join",
+        payload:{
+          roomId: "red"
+        }
+      }))
+    }
+  },[])
+
+  return <div className="h-dvh w-dvw bg-slate-950 flex justify-center items-center">
+    <div className=" h-[95vh] rounded-2xl w-full max-w-4xl mx-auto bg-slate-800 flex flex-1 flex-col p-4 overflow-auto">
+      <div className="p-2 text-white bg-transparent backdrop-blur-2xl rounded-t-2xl ">
+        <h1 className="text-4xl">GlobeChat</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="flex flex-col items-end flex-1  text-white">
+        {messages.map( (m) =>
+          <p  className="bg-slate-500 px-4 py-3 rounded-xl text-base md:text-lg">{m}</p>
+        )}
+        
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div className="flex">
+        <input ref={inputRef} className="bg-slate-100 w-full rounded-l-2xl p-4 text-base md:text-lg" type="text" />
+        <button onClick={()=>{
+          const message = inputRef.current?.value
+          wsRef.current.send(JSON.stringify({
+            type: "chat",
+            payload: {
+              message: message
+            }
+          }))
+          // console.log(inputRef.current?.value)
+          console.log(messages)
+          // console.log(wsRef.current)
+        }} className="bg-green-400 px-4 py-2 rounded-r-2xl cursor-pointer hover:opacity-80">Send</button>
+      </div>
+    </div>
+  </div>
 }
 
 export default App
